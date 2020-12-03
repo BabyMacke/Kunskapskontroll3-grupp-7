de441b9fffb69571a950b8c8e7ef53acd43bd330
StefanHbg <70539225+StefanHbg@users.noreply.github.com>
2020-12-03 14:51:55 +0100

    added files

---
+++ JS/api.js
@@ -0,0 +1,18 @@
+const key = 'b9f8285a5b770fe763736df90c7f8be3';
+let searchTheme = 'Fruits';
+
+const url = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${searchTheme}&format=json&nojsoncallback=1&per_page=12&page=1`;
+
+fetch(url).then(function(response){
+    return response.json();
+}
+).then(function(data){
+    const imgArr = data.photos.photo;
+    shuffleAndDoubler(imgArr);
+}
+//min felhantering.
+).catch(function(error){
+    
+    let errorMsg = document.createElement('h2');
+    errorMsg.textContent = 'Något blev fel. Det kan vara så att Flickr ligger nere eller att du behöver refresha sidan';
+})
---
+++ JS/renderfunc.js
@@ -0,0 +1,13 @@
+function renderMemoryFunc(cardsStorer){
+    let gridContainer = document.querySelector('.grid-container');
+    for (let i = 0; i < cardsStorer.length; i++) {
+        let memoryCard = document.createElement('img');
+        memoryCard.src = cardsStorer[i].url;
+        
+        memoryCard.setAttribute('pair-id', cardsStorer[i].id);
+        console.log(memoryCard);
+        memoryCard.classList.add('cards');
+        gridContainer.appendChild(memoryCard);
+    }
+}
+renderMemoryFunc(obj);
